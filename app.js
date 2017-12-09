c69ebed6d1b0a355c11423239be3bba9e1ae8a6b
ysatishkumar <satish.yaramala@gmail.com>
2017-12-09 18:25:17 +0530

    added header

--- app.js
+++ app.js
@@ -4,15 +4,27 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var $ = require("jquery");
+
+
+// New Code
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('192.168.0.10:27017/ems');
+
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+// var doctor = require('./routes/doctor');
+// var patient = require('./routes/patient');
+// var admin = require('./routes/admin');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var exphbs = require('express-handlebars');
+app.engine('.hbs', exphbs({defaultLayout: 'single', extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +34,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+app.use('/',index);
+// app.use('/doctor', doctor);
+// app.use('/patient',patient);
+// app.use('/admin', admin);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
